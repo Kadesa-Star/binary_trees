eceb773290d663351b15323c6245abf8f231eee4
Kadesa-Star <133206743+Kadesa-Star@users.noreply.github.com>
2024-06-10 12:30:05 +0000

    updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -70,6 +70,8 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+size_t dequeue(binary_tree_t ***queue, size_t size);
+size_t enqueue(binary_tree_t ***queue, binary_tree_t *node, size_t size);
 
 void binary_tree_print(const binary_tree_t *);
 
